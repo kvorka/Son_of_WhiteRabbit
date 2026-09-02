23bd6b5c1af26b10b02dbd8b818c3ef9c235f2b4
kvorka <jkdarkorbit@gmail.com>
2026-09-02 14:25:13 +0200

    fx5 in fortran version rewritten

--- kernels/latgrid/mod/fft/smod/codelets/fourier_transform@fx5a.f90
+++ kernels/latgrid/mod/fft/smod/codelets/fourier_transform@fx5a.f90
@@ -3,8 +3,9 @@ submodule (fourier_transform) fx5a
   
   module procedure fxzm5a
     integer        :: i0, i1, i2, i3, i4
-    real(kind=dbl) :: x0re, x0im, x1re, x1im, x2re, x2im, x3re, x3im, x4re, x4im, &
-                    & t1re, t1im, t2re, t2im, t3re, t3im, t4re, t4im
+    real(kind=dbl) :: t1re, t1im, t2re, t2im, t3re, t3im, t4re, t4im, &
+                      x0re, x0im, x1re, x1im, x2re, x2im, x3re, x3im, &
+                      x4re, x4im, x01,  x02,  x03,  x04
     
     do i4 = 0, k-1
       i0 = 4 * i4
@@ -23,47 +24,95 @@ submodule (fourier_transform) fx5a
           do i1 = 1, 4
             !$omp simd
             do i0 = 1, ndbl
-              x1re = t1re * x(i0,i1,i2,1,i3,1,i4) - t1im * x(i0,i1,i2,2,i3,1,i4)
-              x1im = t1re * x(i0,i1,i2,2,i3,1,i4) + t1im * x(i0,i1,i2,1,i3,1,i4)
-              x2re = t2re * x(i0,i1,i2,1,i3,2,i4) - t2im * x(i0,i1,i2,2,i3,2,i4)
-              x2im = t2re * x(i0,i1,i2,2,i3,2,i4) + t2im * x(i0,i1,i2,1,i3,2,i4)
+              x0re = x(i0,i1,i2,1,i3,1,i4)
+              x0im = x(i0,i1,i2,2,i3,1,i4)
               
-              x3re = x2re - t3re * x(i0,i1,i2,1,i3,3,i4) + t3im * x(i0,i1,i2,2,i3,3,i4)
-              x3im = x2im - t3re * x(i0,i1,i2,2,i3,3,i4) - t3im * x(i0,i1,i2,1,i3,3,i4)
-              x0re = x1re - t4re * x(i0,i1,i2,1,i3,4,i4) + t4im * x(i0,i1,i2,2,i3,4,i4)
-              x0im = x1im - t4re * x(i0,i1,i2,2,i3,4,i4) - t4im * x(i0,i1,i2,1,i3,4,i4)
+              x1re = t1re * x0re
+              x1im = t1re * x0im
               
-              x1re =  2  * x1re -       x0re
-              x1im =  2  * x1im -       x0im
-              x4re =  2  * x2re -       x3re
-              x4im =  2  * x2im -       x3im
-              x2re =       x0re + C53 * x3re
-              x2im =       x0im + C53 * x3im
-              x3re = C53 * x0re -       x3re
-              x3im = C53 * x0im -       x3im
-              x0re =       x1re +       x4re
-              x0im =       x1im +       x4im
+              x3re = x(i0,i1,i2,1,i3,2,i4)
+              x3im = x(i0,i1,i2,2,i3,2,i4)
               
-              x1re = x1re                  -       x4re
-              x1im = x1im                  -       x4im
-              x4re = x(i0,i1,i2,1,i3,0,i4) - C51 * x0re
-              x4im = x(i0,i1,i2,2,i3,0,i4) - C51 * x0im
+              x2re = t2re * x3re
+              x2im = t2re * x3im
               
-              x1re =     x4re - C52 * x1re
-              x1im =     x4im - C52 * x1im
-              x4re = 2 * x4re -       x1re
-              x4im = 2 * x4im -       x1im
+              x1re = x1re - t1im * x0im
+              x1im = x1im + t1im * x0re
+              x2re = x2re - t2im * x3im
+              x2im = x2im + t2im * x3re
               
-              x(i0,i1,i2,1,i3,0,i4) =     x(i0,i1,i2,1,i3,0,i4) +       x0re
-              x(i0,i1,i2,2,i3,0,i4) =     x(i0,i1,i2,2,i3,0,i4) +       x0im
-              x(i0,i1,i2,1,i3,3,i4) =     x1re                  - C54 * x3im
-              x(i0,i1,i2,2,i3,3,i4) =     x1im                  + C54 * x3re
-              x(i0,i1,i2,1,i3,2,i4) = 2 * x1re                  -       x(i0,i1,i2,1,i3,3,i4)
-              x(i0,i1,i2,2,i3,2,i4) = 2 * x1im                  -       x(i0,i1,i2,2,i3,3,i4)
-              x(i0,i1,i2,1,i3,4,i4) =     x4re                  - C54 * x2im
-              x(i0,i1,i2,2,i3,4,i4) =     x4im                  + C54 * x2re
-              x(i0,i1,i2,1,i3,1,i4) = 2 * x4re                  -       x(i0,i1,i2,1,i3,4,i4)
-              x(i0,i1,i2,2,i3,1,i4) = 2 * x4im                  -       x(i0,i1,i2,2,i3,4,i4)
+              x01 = x(i0,i1,i2,1,i3,3,i4)
+              x02 = x(i0,i1,i2,2,i3,3,i4)
+              
+              x3re = x2re - t3re * x01
+              x3im = x2im - t3re * x02
+              
+              x03 = x(i0,i1,i2,1,i3,4,i4)
+              x04 = x(i0,i1,i2,2,i3,4,i4)
+              
+              x0re = x1re - t4re * x03
+              x0im = x1im - t4re * x04
+              
+              x3re = x3re + t3im * x02
+              x3im = x3im - t3im * x01
+              x0re = x0re + t4im * x04
+              x0im = x0im - t4im * x03
+              
+              x1re = 2 * x1re - x0re
+              x1im = 2 * x1im - x0im
+              x4re = 2 * x2re - x3re
+              x4im = 2 * x2im - x3im
+              
+              x2re = x0re + C53 * x3re
+              x2im = x0im + C53 * x3im
+              
+              x3re = C53 * x0re - x3re
+              x3im = C53 * x0im - x3im
+              
+              x0re = x1re + x4re
+              x0im = x1im + x4im
+              
+              x1re = x1re - x4re
+              x1im = x1im - x4im
+              
+              x4re = x(i0,i1,i2,1,i3,0,i4)
+              x4im = x(i0,i1,i2,2,i3,0,i4)
+              
+              x01 = x4re + x0re
+              x02 = x4im + x0im
+              
+              x(i0,i1,i2,1,i3,0,i4) = x01
+              x(i0,i1,i2,2,i3,0,i4) = x02
+              
+              x4re = x4re - C51 * x0re
+              x4im = x4im - C51 * x0im
+              
+              x1re = x4re - C52 * x1re
+              x1im = x4im - C52 * x1im
+              
+              x4re = 2 * x4re - x1re
+              x4im = 2 * x4im - x1im
+              x3im = x1re - C54 * x3im
+              x3re = x1im + C54 * x3re
+              
+              x(i0,i1,i2,1,i3,3,i4) = x3im
+              x(i0,i1,i2,2,i3,3,i4) = x3re
+              
+              x1re = 2 * x1re - x3im
+              x1im = 2 * x1im - x3re
+              x2im = x4re - C54 * x2im
+              x2re = x4im + C54 * x2re
+              
+              x(i0,i1,i2,1,i3,2,i4) = x1re
+              x(i0,i1,i2,2,i3,2,i4) = x1im
+              
+              x4re = 2 * x4re - x2im
+              x4im = 2 * x4im - x2re
+              
+              x(i0,i1,i2,1,i3,4,i4) = x2im
+              x(i0,i1,i2,2,i3,4,i4) = x2re
+              x(i0,i1,i2,1,i3,1,i4) = x4re
+              x(i0,i1,i2,2,i3,1,i4) = x4im
             end do
           end do
         end do
--- shell/gfortran.sh
+++ shell/gfortran.sh
@@ -6,7 +6,10 @@ fcompile="gfortran-12 -Ofast \
                       -march=native \
                       -mno-vzeroupper \
                       -mprefer-vector-width=512 \
+                      -finline-functions \
                       -fno-bounds-check \
+                      -fno-tree-loop-distribute-patterns \
+                      -fcx-limited-range \
                       -fargument-noalias-global \
                       -fstrict-aliasing \
                       -fomit-frame-pointer \
@@ -17,12 +20,16 @@ fcompile="gfortran-12 -Ofast \
                       -D$memory \
                       -D$kernel \
                       -D$code_type \
+                      --param=max-inline-insns-single=5000 \
+                      --param=inline-unit-growth=1000 \
+                      --param=large-function-growth=2000 \
                       -cpp"
 
 ccompile="gcc-12 -Ofast \
                  -march=native \
                  -mno-vzeroupper \
                  -fno-bounds-check \
+                 -fno-tree-loop-distribute-patterns \
                  -fstrict-aliasing \
                  -fomit-frame-pointer \
                  -fno-stack-protector \
